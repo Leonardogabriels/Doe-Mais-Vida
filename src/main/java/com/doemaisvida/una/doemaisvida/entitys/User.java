44ef42f75a5ea469e87c850e33b48fced15e4837
Leonardo Gabriel <leonardogabrielsilva1@outlook.com>
2024-05-10 16:37:45 -0300

    Entity User implements

--- src/main/java/com/doemaisvida/una/doemaisvida/entitys/User.java
+++ src/main/java/com/doemaisvida/una/doemaisvida/entitys/User.java
@@ -1,4 +1,45 @@
 package com.doemaisvida.una.doemaisvida.entitys;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
 public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@NotNull
+	private Long id;
+
+	@NotNull
+	@Column(length = 50)
+	private String name;
+
+	@Email
+	@Column(unique = true)
+	private String email;
+
+	@NotNull
+	@Size(min = 6, max = 255)
+	private String password;
+
+	@NotNull
+	@Size(min = 6, max = 255)
+	@Column(name = "password_confirm")
+	private String passwordConfirm;
+
+	private String bloodType;
+	private String location;
+	private Double weight;
+
 }
